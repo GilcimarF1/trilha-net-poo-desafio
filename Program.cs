f0102ea8943ae2469841cd5bb8b7dcb137f390ae
Gilcimar Salviano <lyonrx@hotmail.com>
2024-01-05 13:43:00 -0300

    Desafio feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+  static void Main(string[] args)
+  {
+    // Nokia
+    Nokia nokia = new Nokia("923568982", "Nokia ModeloTIJOLO", "IMEI555", 64);
+    nokia.Ligar();
+    nokia.ReceberLigacao();
+    nokia.InstalarAplicativo("WhatsApp");
+
+    Console.WriteLine();
+
+    // Iphone
+    Iphone iphone = new Iphone("965983265", "iPhone ModeloSEDA", "IMEI666", 128);
+    iphone.Ligar();
+    iphone.ReceberLigacao();
+    iphone.InstalarAplicativo("Telegram");
+
+    Console.ReadLine();
+  }
+}
